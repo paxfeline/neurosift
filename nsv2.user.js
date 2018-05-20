cb1a124207eaae127b03461c7a551aee40069746
David Newberry <paxfeline@gmail.com>
2018-05-20 00:18:24 -0700

    Add files via upload
    Just fixing tabs.

--- nsv2.user.js
+++ nsv2.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name        NeuroSift
-// @namespace   net.pax.dev
-// @include     http://neurosynth.org/studies/*
-// @version     2
+// @name				NeuroSift
+// @namespace		net.pax.dev
+// @include			http://neurosynth.org/studies/*
+// @version			2
 // ==/UserScript==
 
 // NeuroSift Copyright © 2018 David Newberry and Megan Finnegan, all rights reserved.
@@ -22,10 +22,10 @@ ORIG.forEach( function ( el, ind, arr ) { arr[ind] = parseFloat( el ); } );
 
 while ( ORIG.length != 3 || isNaN( ORIG[0] ) || isNaN( ORIG[1] ) || isNaN( ORIG[2] ) )
 {
-  ORIG = prompt( "Poorly formed input.\nRe-enter coordinates to search around (in default units).", "-48, 40, -6" ).split( "," );
+	ORIG = prompt( "Poorly formed input.\nRe-enter coordinates to search around (in default units).", "-48, 40, -6" ).split( "," );
 
-  // explicitly convert strings to numbers
-  ORIG.forEach( function ( el, ind, arr ) { arr[ind] = parseFloat( el ); } );
+	// explicitly convert strings to numbers
+	ORIG.forEach( function ( el, ind, arr ) { arr[ind] = parseFloat( el ); } );
 }
 
 // ask user for threshold distance
@@ -33,7 +33,7 @@ while ( ORIG.length != 3 || isNaN( ORIG[0] ) || isNaN( ORIG[1] ) || isNaN( ORIG[
 THRESH = parseFloat( prompt( "Enter threshold (in default units).", "8" ) );
 
 while ( isNaN( THRESH ) )
-  THRESH = parseFloat( prompt( "Poorly formed input.\nEnter threshold (in default units).", "8" ) );
+	THRESH = parseFloat( prompt( "Poorly formed input.\nEnter threshold (in default units).", "8" ) );
 
 // begin repetatively calling a function to read in all points
 
@@ -51,79 +51,79 @@ setTimeout(
 
 		if ( info )
 		{
-      // when not fully loaded, may read '0 to 0 of 0'
+			// when not fully loaded, may read '0 to 0 of 0'
 			var re = /Showing (\d+) to (\d+) of (\d+) entries/gi;
 			var found = re.exec( info.innerHTML );
-      
-      // mode 0/1 is used to ensure we start on page 1
-      if ( found[1] > 0 && mode == 0 )
-      {
-        if ( found[1] > 1 )
-        {
-          //alert( "downshift" );
-          
-          // click the "first" button
+			
+			// mode 0/1 is used to ensure we start on page 1
+			if ( found[1] > 0 && mode == 0 )
+			{
+				if ( found[1] > 1 )
+				{
+					//alert( "downshift" );
+					
+					// click the "first" button
 					var el = document.querySelector( "#study_peaks_table_first" );
 					var e = new Event('click');
 					el.dispatchEvent(e);
-          
-          // by setting lastStart here, we ensure that the subsequent conditional will be triggered after turning from whatever page now specified [to page 1]
-          lastStart = found[1];
-        }
-        
-        mode = 1;
-      }
-      else if ( mode == 1 )
-      {
+					
+					// by setting lastStart here, we ensure that the subsequent conditional will be triggered after turning from whatever page now specified [to page 1]
+					lastStart = found[1];
+				}
+				
+				mode = 1;
+			}
+			else if ( mode == 1 )
+			{
 			
-        if ( found[1] > 0 && ( !lastStart || lastStart != found[1] ) )
-        {
-          lastStart = found[1];
+				if ( found[1] > 0 && ( !lastStart || lastStart != found[1] ) )
+				{
+					lastStart = found[1];
 
-          // iterate through each row in the table
-          var rows = document.querySelectorAll( "#study_peaks_table tbody tr" );
+					// iterate through each row in the table
+					var rows = document.querySelectorAll( "#study_peaks_table tbody tr" );
 
-          for ( var row of rows )
-          {
-            var line = new Array();
-            var cs = row.querySelectorAll( "td" );
+					for ( var row of rows )
+					{
+						var line = new Array();
+						var cs = row.querySelectorAll( "td" );
 
-            // put each table cell's text for this row into an array
-            for ( var i = 1; i < cs.length; i++ )
-              line.push( cs[i].textContent );
+						// put each table cell's text for this row into an array
+						for ( var i = 1; i < cs.length; i++ )
+							line.push( cs[i].textContent );
 
-            unsafeWindow.console.log( "line", line );
+						unsafeWindow.console.log( "line", line );
 
-            // use the constructed array (which contains primarily a set of coordinates) to find distance to 'ORIG' point
-            var d = dist3( ORIG, line );
+						// use the constructed array (which contains primarily a set of coordinates) to find distance to 'ORIG' point
+						var d = dist3( ORIG, line );
 
-            unsafeWindow.console.log( "d", d );
+						unsafeWindow.console.log( "d", d );
 
-            // if the distance is less than THRESH, add this point to those being outputted
-            if ( d <= THRESH )
-            {
-              line.push( " table " + cs[0].textContent );
-              o.push( line.join( "," ) );
-            }
-          }
+						// if the distance is less than THRESH, add this point to those being outputted
+						if ( d <= THRESH )
+						{
+							line.push( " table " + cs[0].textContent );
+							o.push( line.join( "," ) );
+						}
+					}
 
-          // found[2] == found[3] means the final page has been reached, and the script can finish
-          if ( found[2] == found[3] )
-            flag = true;
-          else
-          {
-            // click the "next" button
-            var el = document.querySelector( "#study_peaks_table_next" );
-            var e = new Event('click');
-            el.dispatchEvent(e);
-          }
-        }
-      }
-      
-    }
+					// found[2] == found[3] means the final page has been reached, and the script can finish
+					if ( found[2] == found[3] )
+						flag = true;
+					else
+					{
+						// click the "next" button
+						var el = document.querySelector( "#study_peaks_table_next" );
+						var e = new Event('click');
+						el.dispatchEvent(e);
+					}
+				}
+			}
+			
+		}
 
-    // if done, output.
-    // else, set timeout to call this function again.
+		// if done, output.
+		// else, set timeout to call this function again.
 		if ( flag )
 		{
 			var ta = document.createElement( "textarea" );
@@ -141,10 +141,10 @@ setTimeout(
 /* 
 	dist3 takes two inputs,
 		each an arry with [at least] 3 elements; call them
-    	ar1 = [ x1, y1, z1 ],
-    	ar2 = [ x2, y2, z2 ]
-  Returns:
-   	sqrt( (x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2 )
+			ar1 = [ x1, y1, z1 ],
+			ar2 = [ x2, y2, z2 ]
+	Returns:
+		sqrt( (x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2 )
 */
 
 function dist3( ar1, ar2 )
